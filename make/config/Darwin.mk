7ce6a8a59c8ada8d53fd3c2de22799806a77c8e1
clynejp <>
2007-01-05 21:39:18 +0000

    Makefiles assumed NETCDF_INC_PATH variable set, which may not be the case

--- make/config/Darwin.mk
+++ make/config/Darwin.mk
@@ -25,11 +25,11 @@ G++-INCLUDE-DIR = /usr/include/g++
 CXX = c++ -fno-common
 CC = cc -fno-common
 
-CXXFLAGS          += -DDARWIN -Wall -Werror -Wno-format -Wno-sign-compare  -fPIC
+CXXFLAGS          += -DDARWIN -Wall -Wno-format -Wno-sign-compare  -fPIC
 CXX_RELEASE_FLAGS += -O3 -DNDEBUG
 CXX_DEBUG_FLAGS   += -g
 
-CFLAGS            += -DDARWIN -Wall -Werror -Wno-format -fPIC
+CFLAGS            += -DDARWIN -Wall -Wno-format -fPIC
 C_RELEASE_FLAGS   += -O3 -DNDEBUG
 C_DEBUG_FLAGS     += -g
 
