166dcb2498c42c6e54fcf68f41a94b6672e99750
alannorton <>
2007-03-09 17:25:19 +0000

    Numerous fixes to context-sensitive help.

--- apps/vaporgui/guis/floweventrouter.h
+++ apps/vaporgui/guis/floweventrouter.h
@@ -57,7 +57,7 @@ public:
 	virtual void cleanParams(Params* p); 
 	
 	void fileLoadTF(FlowParams* dParams);
-	void sessionLoadTF(QString* name) {assert(0);}  
+	void sessionLoadTF(QString*) {assert(0);}  
 	void fileSaveTF(FlowParams* dParams);
 		
 	//There are multiple notions of "dirty" here!
