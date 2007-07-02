7bbda44702b98c14159ca47734a12e415ec9e1e5
clynejp <>
2007-07-02 22:15:25 +0000

    *** empty log message ***

---
+++ include/vapor/MetadataSpherical.h
@@ -0,0 +1,53 @@
+//
+//      $Id$
+//
+
+
+#ifndef	_MetadataSpherical_h_
+#define	_MetadataSpherical_h_
+
+#include "vapor/Metadata.h"
+#ifdef WIN32
+#pragma warning(disable : 4251)
+#endif
+
+namespace VAPoR {
+
+
+//
+//! \class MetadataSpherical
+//! \brief A class for managing data set Spherical metadata
+//! \author John Clyne
+//! \version $Revision$
+//! \date    $Date$
+//!
+class VDF_API MetadataSpherical : public Metadata {
+public:
+
+ MetadataSpherical(
+	const size_t dim[3], size_t numTransforms, size_t bs[3],
+	size_t permutation[3],
+	int nFilterCoef = 1, int nLiftingCoef = 1, int msbFirst =  1,
+	int vdfVersion = VDF_VERSION
+	);
+
+ MetadataSpherical(const string &path);
+
+ virtual ~MetadataSpherical();
+
+protected: 
+	int SetDefaults();
+private:
+ int _permutation[3];
+
+ // Known xml tags
+ //
+ static const string _gridPermutationTag;
+
+
+};
+
+
+};
+
+#endif	//	_MetadataSpherical_h_
