1d047da926e23e3fda87e4ca551da660686ad1cf
clynejp <>
2007-10-24 21:42:13 +0000

    Updated version number to 1.2

--- include/vapor/Version.h
+++ include/vapor/Version.h
@@ -77,8 +77,8 @@ public:
 
 private:
  static const int _majorVersion = 1;
- static const int _minorVersion = 1;
- static const int _minorMinorVersion = 2;
+ static const int _minorVersion = 2;
+ static const int _minorMinorVersion = 0;
  static string _formatString;
  static string _dateString;
 
