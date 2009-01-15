6244d52e34b2bee674c9e77ecc17544e3bb99f50
alannorton <>
2009-01-15 21:54:31 +0000

    Added

---
+++ lib/render/customcontext.cpp
@@ -0,0 +1,66 @@
+//************************************************************************
+//																		*
+//		     Copyright (C)  2009										*
+//     University Corporation for Atmospheric Research					*
+//		     All Rights Reserved										*
+//																		*
+//************************************************************************/
+//
+//	File:		customContext.cpp
+//
+//	Author:		Alan Norton
+//			National Center for Atmospheric Research
+//			PO 3000, Boulder, Colorado
+//
+//	Date:		January 2009
+//
+//	Description:	Implementation of the customContext class
+//		This extents the QGLContext, so that we can request
+//		the context to have an Aux Buffer (not present in Mac OS10.5.6)
+//
+#include "customcontext.h"
+using namespace VAPoR;
+#ifdef WIN32
+int CustomContext::choosePixelFormat(void*p, HDC pdc){
+	//Nothing special for windows.
+
+	return QGLContext::choosePixelFormat(p,pdc);
+}
+#elif defined (Q_WS_X11)
+void *CustomContext::chooseVisual() {
+	fprintf(stderr, "in custom context::chooseVisual\n");
+	GLint attribs[] = {GLX_RGBA, GLX_DOUBLEBUFFER, GLX_AUX_BUFFERS, 1, None};
+	XVisualInfo *vis = glXChooseVisual(device()->x11Display(), device()->x11Screen(), attribs);
+	
+	if (vis) {
+		GLint numBufs = 0; 
+		//Check if we got it:
+		glXGetConfig(device()->x11Display(), vis, GLX_AUX_BUFFERS, &numBufs);
+		if (numBufs < 1) {
+			qWarning("no Aux buffers");
+			assert(0);
+		}
+		return vis;
+	}
+	qWarning( "Null returned from glXChooseVisual");
+	assert(0); //Didn't work!!
+	return QGLContext::chooseVisual();
+}
+#endif
+#if defined(Q_WS_MAC)
+void *CustomContext::chooseMacVisual(GDHandle gdev) {
+	GLint attribs[] = {AGL_ALL_RENDERERS, AGL_RGBA, AGL_AUX_BUFFERS, 1, AGL_NONE};
+	AGLPixelFormat fmt = aglChoosePixelFormat(NULL, 0, attribs);
+	if (fmt) {
+		GLint numBufs = 0; 
+		//Check if we got it:
+		aglDescribePixelFormat(fmt, AGL_AUX_BUFFERS, &numBufs);
+		QString txt(" Number of aux buffers: ");
+		txt += QString::number(numBufs);
+		QMessageBox::information(0,"Number of Aux buffers",txt, QMessageBox::Ok);
+		return fmt;
+	}
+	assert(0); //Didn't work!!
+	return QGLContext::chooseMacVisual(gdev);
+}
+#endif
---
+++ lib/render/customcontext.h
@@ -0,0 +1,48 @@
+//************************************************************************
+//																		*
+//		     Copyright (C)  2009										*
+//     University Corporation for Atmospheric Research					*
+//		     All Rights Reserved										*
+//																		*
+//************************************************************************/
+//
+//	File:		customContext.h
+//
+//	Author:		Alan Norton
+//			National Center for Atmospheric Research
+//			PO 3000, Boulder, Colorado
+//
+//	Date:		January 2009
+//
+//	Description:	Definition of the customContext class
+//
+#ifndef CUSTOMCONTEXT_H
+#define CUSTOMCONTEXT_H
+#include <qapplication.h>
+#include <qgl.h>
+#include "common.h"
+#if defined(Q_WS_X11)  //for X11 platforms...
+#include <GL/glx.h> 
+#elif defined(Q_WS_MAC)
+#include <agl.h>
+#endif
+namespace VAPoR {
+	class RENDER_API CustomContext : public QGLContext 
+	{
+	public:
+		CustomContext( const QGLFormat &fmt)
+			: QGLContext(fmt) {}
+	protected:
+#ifdef WIN32
+		int choosePixelFormat(void* p, HDC hdc);
+#elif defined (Q_WS_X11)// X11
+		void *chooseVisual();
+#elif defined (Q_WS_MAC)
+		void *chooseMacVisual(GDHandle gdev);
+#else
+		assert(0); //unsupported platform
+#endif
+	};
+
+}; //end VAPoR namespace
+#endif //CUSTOMCONTEXT_H
