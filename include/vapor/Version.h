d9f8b5e73fe4665e8e7de1d4c6649c5806c3d9c5
alannorton <>
2009-06-10 14:47:54 +0000

    Updated version to 1.5.0

--- include/vapor/Version.h
+++ include/vapor/Version.h
@@ -81,8 +81,8 @@ public:
 
 private:
  static const int _majorVersion = 1;
- static const int _minorVersion = 4;
- static const int _minorMinorVersion = 3;
+ static const int _minorVersion = 5;
+ static const int _minorMinorVersion = 0;
  static string _formatString;
  static string _dateString;
 
