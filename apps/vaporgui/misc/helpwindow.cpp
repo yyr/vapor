bc9962f8498663e51b590e7a597e84b92fafda2a
alannorton <>
2010-11-18 21:26:16 +0000

    Fix for 3111723

--- apps/vaporgui/misc/helpwindow.cpp
+++ apps/vaporgui/misc/helpwindow.cpp
@@ -7,354 +7,131 @@
 ** program may be used, distributed and modified without limitation.
 **
 *****************************************************************************/
-#include "../images/back.xpm"
-#include "../images/forward.xpm"
-#include "../images/home2.xpm"
+#include <QtCore/QLibraryInfo>
+#include <QtGui/QApplication>
+#include <QtGui/QAction>
+#include <QtGui/QMenu>
+#include <QtGui/QMenuBar>
+#include <QtGui/QMessageBox>
+#include <QtCore/QFileInfo>
+#include <QtCore/QFile>
+#include <QFileDialog>
+
 #include "helpwindow.h"
 #include "GetAppPath.h"
-#include <qstatusbar.h>
-#include <QFileDialog>
-#include <QPrintDialog>
-#include <qpixmap.h>
-#include <qimage.h>
-#include <QToolBar>
-#include <QAction>
-#include <qmenubar.h>
-#include <qtoolbutton.h>
-#include <qicon.h>
-#include <qfile.h>
-#include <qmessagebox.h>
-#include <qapplication.h>
-#include <qcombobox.h>
-#include <qevent.h>
-#include <qlineedit.h>
-#include <qobject.h>
-#include <qfileinfo.h>
-#include <qfile.h>
-#include <qdatastream.h>
-#include <qprinter.h>
-#include <qpainter.h>
-
-#include <ctype.h>
-#include <stdlib.h>
-
+#include <vector>
+#include <string>
 
 using namespace VetsUtil;
 
-HelpWindow* HelpWindow::theHelpWindow = 0;
+QString HelpWindow::lastDir;
 
-HelpWindow::HelpWindow( const QString& home_, const QString& _path,
-			QWidget* parent, const char * )
-    : QMainWindow( parent ),
-      pathCombo( 0 )
+HelpWindow::HelpWindow()
 {
-    readHistory();
-    readBookmarks();
-
-    browser = new QTextBrowser( this );
-
-    browser->setSource(_path);
-    connect( browser, SIGNAL( sourceChanged(const QUrl& ) ),
-	     this, SLOT( sourceChanged( const QUrl&) ) );
-
-    setCentralWidget( browser );
-
-    if ( !home_.isEmpty() )
-	browser->setSource( home_ );
-
-    connect( browser, SIGNAL( highlighted( const QUrl&) ),
-	     statusBar(), SLOT( showMessage( const QUrl&)) );
-
-    resize( 900,700 );
-	QMenu* file = menuBar()->addMenu(tr("File"));
-	//Create actions for each menu item, add to file menu:
-	newAct = new QAction(tr("&New Window"), this);
-	newAct->setShortcut(Qt::CTRL+Qt::Key_N);
-	connect(newAct,SIGNAL(triggered()),this, SLOT(newWindow()));
-	file->addAction(newAct);
-
    
-	openAct = new QAction(tr("&Open File"), this);
-	openAct->setShortcut(Qt::CTRL+Qt::Key_O);
-	connect(openAct,SIGNAL(triggered()),this, SLOT(openFile()));
-	file->addAction(openAct);
-   
-	printAct = new QAction(tr("&Print"), this);
-	printAct->setShortcut(Qt::CTRL+Qt::Key_P);
-	connect(printAct,SIGNAL(triggered()),this, SLOT(print()));
-	file->addAction(printAct);
-
-    file->addSeparator();
-   
-	closeAct = new QAction(tr("&Close"), this);
-	closeAct->setShortcut(Qt::CTRL+Qt::Key_Q);
-	connect(closeAct,SIGNAL(triggered()),this, SLOT(close()));
-	file->addAction(closeAct);
-
-    // The same three icons are used twice each.
-	QPixmap* backPix = new QPixmap(back);
-	QPixmap* forwardPix = new QPixmap(forward);
-	QPixmap* homePix = new QPixmap(home2);
-   
-	QIcon icon_back(*backPix);
-    QIcon icon_forward( *forwardPix );
-    QIcon icon_home( *homePix);
-
-    QMenu* go = menuBar()->addMenu(tr("Go")); 
-
-	backAct = new QAction(icon_back,tr("&Backward"), this);
-	backAct->setShortcut(Qt::CTRL+Qt::Key_Left);
-	connect(backAct,SIGNAL(triggered()),this, SLOT(backward()));
-	go->addAction(openAct);
-
-	fwdAct = new QAction(icon_forward,tr("&Forward"), this);
-	fwdAct->setShortcut(Qt::CTRL+Qt::Key_Right);
-	connect(fwdAct,SIGNAL(triggered()),this, SLOT(forward()));
-	go->addAction(fwdAct);
-
-    /*backwardId = go->insertItem( icon_back,
-				 tr("&Backward"), browser, SLOT( backward() ),
-				 Qt::CTRL+Qt::Key_Left );
-    forwardId = go->insertItem( icon_forward,
-				tr("&Forward"), browser, SLOT( forward() ),
-				Qt::CTRL+Qt::Key_Right );
-    go->insertItem( icon_home, tr("&Home"), browser, SLOT( home() ) );
-	*/
-
-    QMenu* help = menuBar()->addMenu(tr("Help"));
-	aboutAct = new QAction(tr("&About"),this);
-	connect(aboutAct,SIGNAL(triggered()),this, SLOT(about()));
-	help->addAction(fwdAct);
-    //help->insertItem( tr("&About"), this, SLOT( about() ) );
-
-    hist = menuBar()->addMenu(tr("History"));
-    QStringList::Iterator it = history.begin();
-	int histIndex = 0;
-	for ( ; it != history.end(); ++it ){
-		hist->addAction( *it );
-		mHistory[histIndex++ ] = *it;
-		connect( hist, SIGNAL( activated( int ) ),
-			this, SLOT( histChosen( int ) ) );
-	}
-
-    QMenu* bookm = menuBar()->addMenu(tr("Bookmarks")); 
-	bookAct = new QAction(tr("Bookmarks"),this);
-	connect(bookAct,SIGNAL(activated()), this, SLOT( addBookmark()));
-    
-    bookm->addSeparator();
-
-    QStringList::Iterator it2 = bookmarks.begin();
-	int bookIndex = 0;
-	for ( ; it2 != bookmarks.end(); ++it2 ){
-		bookm->addAction(*it2);
-		mBookmarks[ bookIndex++ ] = *it2;
-		connect( bookm, SIGNAL( activated( int ) ),
-			this, SLOT( bookmChosen( int ) ) );
-	}
-
-    fwdAct->setEnabled(FALSE);
-	backAct->setEnabled(FALSE);
-	
-    connect( browser, SIGNAL( backwardAvailable( bool ) ),
-	     this, SLOT( setBackwardAvailable( bool ) ) );
-    connect( browser, SIGNAL( forwardAvailable( bool ) ),
-	     this, SLOT( setForwardAvailable( bool ) ) );
-
-
-    QToolBar* toolbar = new QToolBar( this );
-    addToolBar( toolbar);
-    QToolButton* button;
-
-	button = new QToolButton(toolbar);
-	button->setIcon(icon_back);
-	button->setText(tr("Backward"));
-	connect(button, SIGNAL(triggered()), browser, SLOT(backward()));
-    //button = new QToolButton( icon_back, tr("Backward"), "", browser, SLOT(backward()), toolbar );
-    connect( browser, SIGNAL( backwardAvailable(bool) ), button, SLOT( setEnabled(bool) ) );
-    button->setEnabled( FALSE );
-
-	button = new QToolButton(toolbar);
-	button->setIcon(icon_forward);
-	button->setText(tr("Forward"));
-	connect(button, SIGNAL(triggered()), browser, SLOT(forward()));
-   
-    connect( browser, SIGNAL( forwardAvailable(bool) ), button, SLOT( setEnabled(bool) ) );
-    button->setEnabled( FALSE );
-
-   // button = new QToolButton( icon_forward, tr("Forward"), "", browser, SLOT(forward()), toolbar );
-    //connect( browser, SIGNAL( forwardAvailable(bool) ), button, SLOT( setEnabled(bool) ) );
-    //button->setEnabled( FALSE );
-    //button = new QToolButton( icon_home, tr("Home"), "", browser, SLOT(home()), toolbar );
-
-    toolbar->addSeparator();
-
-    pathCombo = new QComboBox(toolbar );
-    connect( pathCombo, SIGNAL( activated( const QString & ) ),
-	     this, SLOT( pathSelected( const QString & ) ) );
-    //toolbar->setStretchableWidget( pathCombo );
-    //setRightJustification( TRUE );
-    //setDockEnabled( Qt::DockLeft, FALSE );
-    //setDockEnabled( Qt::DockRight, FALSE );
-
-    pathCombo->insertItem(0, home_ );
-    browser->setFocus();
+    textViewer = new TextEdit;
+    setCentralWidget(textViewer);
+    createActions();
+    createMenus();
+    setWindowTitle(tr("VAPOR Help Viewer"));
+    resize(750, 400);
 
 }
 
 
-void HelpWindow::showHelp(const QString& filename){
-	
-
-	vector <string> paths;
+void HelpWindow::showHelp(const char* filename){
+	std::vector<std::string>paths;
 	paths.push_back("doc");
-	paths.push_back(filename.toStdString());
-	QString filePath =  GetAppPath("VAPOR", "share", paths).c_str();
-
-	if (!theHelpWindow){
-		theHelpWindow = new HelpWindow(filePath, filePath);
-	} 
-	theHelpWindow->browser->setSource(filePath);
+	lastDir = GetAppPath("VAPOR","share",paths).c_str();
+	paths.push_back(string(filename));
+	QString filePath = GetAppPath("VAPOR","share",paths).c_str();
+	HelpWindow* theHelpWindow = new HelpWindow();
+	theHelpWindow->textViewer->setContents(filePath);
 	theHelpWindow->show();
-}
 
-void HelpWindow::setBackwardAvailable( bool b)
-{
-    backAct->setEnabled(b);
-}
-
-void HelpWindow::setForwardAvailable( bool b)
-{
-    fwdAct->setEnabled(b);
-}
-
-
-void HelpWindow::sourceChanged( const QUrl& url )
-{
-    if ( browser->documentTitle().isNull() )
-	setWindowTitle( "VAPOR Help Viewer - " + url.toString() );
-    else
-	setWindowTitle( "VAPOR Help Viewer - " + browser->documentTitle() ) ;
-
-    if ( !url.isEmpty() && pathCombo ) {
-	bool exists = FALSE;
-	int i;
-	for ( i = 0; i < pathCombo->count(); ++i ) {
-	    if ( pathCombo->itemText( i ) == url.toString() ) {
-		exists = TRUE;
-		break;
-	    }
-	}
-	if ( !exists ) {
-	    pathCombo->insertItem(0, url.toString() );
-	    pathCombo->setCurrentIndex( 0 );
-		hist->addAction(url.toString());
-	    mHistory[ hist->actions().count()-1 ] = url.toString();
-	} else
-	    pathCombo->setCurrentIndex( i );
-    }
-}
-
-HelpWindow::~HelpWindow()
-{
-    history =  mHistory.values();
-
-    QFile f( QDir::currentPath() + "/.history" );
-    f.open( QIODevice::WriteOnly );
-    QDataStream s( &f );
-    s << history;
-    f.close();
-
-    bookmarks = mBookmarks.values();
-
-    QFile f2( QDir::currentPath() + "/.bookmarks" );
-    f2.open( QIODevice::WriteOnly );
-    QDataStream s2( &f2 );
-    s2 << bookmarks;
-    f2.close();
-	theHelpWindow = 0;
 }
 
 void HelpWindow::about()
 {
-    QMessageBox::about( this, "Help Viewer for VAPOR",
-			"<p>This displays context-sensitive help</p>");
-		
+    QMessageBox::about(this, tr("About VAPOR Help Viewer"),
+                       tr("This viewer displays html text\n"
+                          "such as is used to provide Flow setup help."));
 }
 
-void HelpWindow::openFile()
-{
-#ifndef QT_NO_FILEDIALOG
-    QString fn = QFileDialog::getOpenFileName(this, QString::null, QString::null );
 
-    if ( !fn.isEmpty() )
-	browser->setSource( fn );
-#endif
+
+void HelpWindow::open()
+{
+	QString s = QFileDialog::getOpenFileName(this,"Select HTML File to Open",lastDir, "*.html");
+	if (s.isNull()) return;
+	textViewer->setContents(s.toLatin1());
+	lastDir = s;
 }
 
-void HelpWindow::newWindow()
+
+void HelpWindow::createActions()
 {
-    ( new HelpWindow(browser->source().toString(), "qbrowser") )->show();
+
+    openAct = new QAction(tr("&Open..."), this);
+    openAct->setShortcut(QKeySequence::Open);
+    connect(openAct, SIGNAL(triggered()), this, SLOT(open()));
+
+    clearAct = new QAction(tr("&Clear"), this);
+    clearAct->setShortcut(tr("Ctrl+C"));
+    connect(clearAct, SIGNAL(triggered()), textViewer, SLOT(clear()));
+
+    exitAct = new QAction(tr("E&xit"), this);
+    exitAct->setShortcuts(QKeySequence::Quit);
+    connect(exitAct, SIGNAL(triggered()), this, SLOT(close()));
+
+    aboutAct = new QAction(tr("&About"), this);
+    connect(aboutAct, SIGNAL(triggered()), this, SLOT(about()));
 }
 
-void HelpWindow::print()
+
+void HelpWindow::createMenus()
 {
-#ifndef QT_NO_PRINTER
-    QPrinter printer( QPrinter::HighResolution );
-    printer.setFullPage(TRUE);
-	QPrintDialog dialog(&printer, this);
-    if ( dialog.exec() ) {
-		browser->print(&printer);
+    fileMenu = new QMenu(tr("&File"), this);
+    fileMenu->addAction(openAct);
+    fileMenu->addAction(clearAct);
+    fileMenu->addSeparator();
+    fileMenu->addAction(exitAct);
+
+    helpMenu = new QMenu(tr("&Help"), this);
+    helpMenu->addSeparator();
+    helpMenu->addAction(aboutAct);
+    menuBar()->addMenu(fileMenu);
+    menuBar()->addMenu(helpMenu);
+}
+
+
+
+TextEdit::TextEdit(QWidget *parent)
+    : QTextEdit(parent)
+{
+    setReadOnly(true);
+}
+
+void TextEdit::setContents(const QString &fileName)
+{
+    QFileInfo fi(fileName);
+    srcUrl = QUrl::fromLocalFile(fi.absoluteFilePath());
+    QFile file(fileName);
+    if (file.open(QIODevice::ReadOnly)) {
+        QString data(file.readAll());
+        if (fileName.endsWith(".html"))
+            setHtml(data);
+        else
+            setPlainText(data);
     }
-#endif
 }
 
-void HelpWindow::pathSelected( const QString &_path )
+QVariant TextEdit::loadResource(int type, const QUrl &name)
 {
-    browser->setSource( _path );
-	if ( mHistory.values().contains(_path) ){
-		hist->addAction(_path);
-		mHistory[ hist->actions().count()-1 ] = _path;
-	}
-}
-
-void HelpWindow::readHistory()
-{
-    if ( QFile::exists( QDir::currentPath() + "/.history" ) ) {
-	QFile f( QDir::currentPath() + "/.history" );
-	f.open( QIODevice::ReadOnly );
-	QDataStream s( &f );
-	s >> history;
-	f.close();
-	while ( history.count() > 20 )
-	    history.erase( 0 );
+    if (type == QTextDocument::ImageResource) {
+        QFile file(srcUrl.resolved(name).toLocalFile());
+        if (file.open(QIODevice::ReadOnly))
+            return file.readAll();
     }
-}
-
-void HelpWindow::readBookmarks()
-{
-    if ( QFile::exists( QDir::currentPath() + "/.bookmarks" ) ) {
-	QFile f( QDir::currentPath() + "/.bookmarks" );
-	f.open( QIODevice::ReadOnly );
-	QDataStream s( &f );
-	s >> bookmarks;
-	f.close();
-    }
-}
-
-void HelpWindow::histChosen( int i )
-{
-    if ( mHistory.contains( i ) )
-	browser->setSource( mHistory[ i ] );
-}
-
-void HelpWindow::bookmChosen( int i )
-{
-    if ( mBookmarks.contains( i ) )
-	browser->setSource( mBookmarks[ i ] );
-}
-
-void HelpWindow::addBookmark()
-{
-	bookm->addAction(windowTitle());
-    mBookmarks[ bookm->actions().count()-1 ] = browser->source().toString();
+    return QTextEdit::loadResource(type, name);
 }
--- apps/vaporgui/misc/helpwindow.h
+++ apps/vaporgui/misc/helpwindow.h
@@ -1,8 +1,9 @@
 /****************************************************************************
 ** QT example code, modified for use in VAPOR by A. Norton 1/10/07
+** Modified again in 2010 based on TextEdit example in QT 4.6
 ** $Id$
 **
-** Copyright (C) 1992-2000 Trolltech AS.  All rights reserved.
+** Copyright (C) 1992-2010 Trolltech AS.  All rights reserved.
 **
 ** This file is part of an example program for Qt.  This example
 ** program may be used, distributed and modified without limitation.
@@ -12,59 +13,49 @@
 #ifndef HELPWINDOW_H
 #define HELPWINDOW_H
 
-#include <QTextBrowser>
-#include <QMainWindow>
-#include <qstringlist.h>
-#include <qmap.h>
-#include <qdir.h>
-#include <QMenu>
-//Added by qt3to4:
-
-class QComboBox;
-class QMenu;
+#include <QtGui/QMainWindow>
+#include <QtCore/QUrl>
+#include <QtGui/QTextEdit>
+class TextEdit;
 
 class HelpWindow : public QMainWindow
 {
     Q_OBJECT
+
 public:
-    HelpWindow( const QString& home_,  const QString& path, QWidget* parent = 0, const char *name=0 );
-    ~HelpWindow();
-	static void showHelp(const QString& filename);
+    HelpWindow();
+	static void showHelp(const char*);
+	TextEdit *textViewer;
 
 private slots:
-    void setBackwardAvailable( bool );
-    void setForwardAvailable( bool );
-
-    void sourceChanged( const QUrl& );
     void about();
-   
-    void openFile();
-    void newWindow();
-    void print();
+    void open();
 
-    void pathSelected( const QString & );
-    void histChosen( int );
-    void bookmChosen( int );
-    void addBookmark();
 
 private:
-	static HelpWindow* theHelpWindow;
-    void readHistory();
-    void readBookmarks();
-	QAction* newAct, *openAct, *printAct, *closeAct;
-	QAction* backAct, *fwdAct, *aboutAct, *bookAct;
-    QTextBrowser* browser;
-    QComboBox *pathCombo;
-    int backwardId, forwardId;
-    QStringList history, bookmarks;
-    QMap<int, QString> mHistory, mBookmarks;
-    QMenu *hist, *bookm;
+	static QString lastDir;
+    void createActions();
+    void createMenus();
 
+    QMenu *fileMenu;
+    QMenu *helpMenu;
+
+    QAction *clearAct;
+    QAction *openAct;
+    QAction *exitAct;
+    QAction *aboutAct;
 };
 
+class TextEdit : public QTextEdit
+{
+    Q_OBJECT
 
+public:
+    TextEdit(QWidget *parent = 0);
+    void setContents(const QString &fileName);
 
-
-
+private:
+    QVariant loadResource(int type, const QUrl &name);
+    QUrl srcUrl;
+};
 #endif
-
