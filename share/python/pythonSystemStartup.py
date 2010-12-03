d792aebb328e9d187625e84654aec9591e3e42a5
alannorton <>
2010-12-03 22:45:07 +0000

    Fix for curl operator

--- share/python/pythonSystemStartup.py
+++ share/python/pythonSystemStartup.py
@@ -6,7 +6,6 @@ def mag3d(a1,a2,a3):
 def mag2d(a1,a2): 
 	return sqrt(a1*a1 + a2*a2)
 
-
 # NAME:
 #       DERIV_FINDIFF
 #
@@ -34,7 +33,7 @@ def deriv_findiff(a,dir,dx):
 	#
 	# derivative for dir=1
 	#
-	if dir == 1: 
+	if dir == 3: 
 
 		#forward differences near the first boundary
 		for i in range(3):
@@ -64,7 +63,7 @@ def deriv_findiff(a,dir,dx):
 	#
 	# derivative for dir=3
 	#
-	if dir == 3:
+	if dir == 1:
 		for i in range(3):
 			aprime[i,:,:] = (-11*a[i,:,:]+18*a[i+1,:,:]-9*a[i+2,:,:]+2*a[i+3,:,:]) /(6*dx)     #forward differences near the first boundary
 
@@ -76,47 +75,29 @@ def deriv_findiff(a,dir,dx):
 
 	return aprime
 
+def curl_findiff(A,B,C):
 
+	ext = (__BOUNDS__[3]-__BOUNDS__[0], __BOUNDS__[4]-__BOUNDS__[1],__BOUNDS__[5]-__BOUNDS__[2])
+	print ext
+	usrmax = vapor.MapVoxToUser([__BOUNDS__[3],__BOUNDS__[4],__BOUNDS__[5]],__REFINEMENT__)
+	usrmin = vapor.MapVoxToUser([__BOUNDS__[0],__BOUNDS__[1],__BOUNDS__[2]],__REFINEMENT__)
+	print usrmax, usrmin
+	dz = (usrmax[2]-usrmin[2])/ext[2]	# grid delta in Python coord system
+	dy =  (usrmax[1]-usrmin[1])/ext[1]
+	dx =  (usrmax[0]-usrmin[0])/ext[0]
+	
+#  in Python coordinates, C,B,A are X,Y,Z components of vector field
+	aux1 = deriv_findiff(A,2,dy)       #x component of the curl (in python system)
+	aux2 = deriv_findiff(B,3,dz)
+	outx = aux2-aux1						#is -z component in user coordinates...
 
-#
-# NAME:
-#       CURL_FINDIFF
-#
-# PURPOSE:
-#       Computes the curl of a vector field using sixth
-#       order finite differences in regular Cartesian grids
-#
-# CALLING SEQUENCE:
-#       OUTX,OUTY,OUTZ = CURL_FINDIFF,INX,INY,INZ,DX,DY,DZ
-#
-# PARAMETERS:
-#       INX[in]:   3D array with the x component of the field
-#       INY[in]:   3D array with the y component of the field
-#       INZ[in]:   3D array with the z component of the field
-#       DX[in]:    spatial step of the grid in the x direction
-#       DY[in]:    spatial step of the grid in the y direction.
-#       DZ[in]:    spatial step of the grid in the z direction.
-#
-# RETURNS:
-#       OUTX[out]: 3D array with the x component of the curl
-#       OUTY[out]: 3D array with the y component of the curl
-#       OUTZ[out]: 3D array with the z component of the curl
-#
-#
-#
-def curl_findiff(inx,iny,inz,dx,dy,dz):
+	aux1 = deriv_findiff(C,3,dz)       #y component of the curl
+	aux2 = deriv_findiff(A,1,dx)
+	outy = aux2-aux1
 
-	aux1 = deriv_findiff(inz,2,dy)       #x component of the curl
-	aux2 = deriv_findiff(iny,3,dz)
-	outx = aux1-aux2
-
-	aux1 = deriv_findiff(inx,3,dz)       #y component of the curl
-	aux2 = deriv_findiff(inz,1,dx)
-	outy = aux1-aux2
-
-	aux1 = deriv_findiff(iny,1,dx)       #z component of the curl
-	aux2 = deriv_findiff(inx,2,dy)
-	outz = aux1-aux2
-
-	return outx, outy, outz
+	aux1 = deriv_findiff(B,1,dx)       #z component of the curl
+	aux2 = deriv_findiff(C,2,dy)
+	outz = aux2-aux1
+# now reverse order of results to go back to user coordinates
+	return outz, outy, outx
 
