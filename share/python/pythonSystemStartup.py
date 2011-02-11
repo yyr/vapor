a9f2fbd302745f32952194edeadfa0f339f7a87f
clynejp <>
2011-02-11 17:02:04 +0000

    Made parameter value meanings for deriv_findiff consistent with IDL version of script

--- share/python/pythonSystemStartup.py
+++ share/python/pythonSystemStartup.py
@@ -18,7 +18,7 @@ def mag2d(a1,a2):
 #
 # PARAMETERS:
 #       A[in]:   3D array with the field component
-#       DIR[in]:  direction (1,2, or 3) of the the derivative
+#       DIR[in]:  direction (1=X,2=Y, or 3=Z) of the the derivative
 #       DX[in]:   spatial step of the grid in the direction DIR
 #
 # RETURNS:       
@@ -33,7 +33,7 @@ def deriv_findiff(a,dir,dx):
 	#
 	# derivative for dir=1
 	#
-	if dir == 3: 
+	if dir == 1: 
 
 		#forward differences near the first boundary
 		for i in range(3):
@@ -63,7 +63,7 @@ def deriv_findiff(a,dir,dx):
 	#
 	# derivative for dir=3
 	#
-	if dir == 1:
+	if dir == 3:
 		for i in range(3):
 			aprime[i,:,:] = (-11*a[i,:,:]+18*a[i+1,:,:]-9*a[i+2,:,:]+2*a[i+3,:,:]) /(6*dx)     #forward differences near the first boundary
 
@@ -89,15 +89,15 @@ def curl_findiff(A,B,C):
 #  in Python coordinates, C,B,A are X,Y,Z components of vector field
 	aux1 = deriv_findiff(A,2,dy)       #x component of the curl (in python system)
 	aux2 = deriv_findiff(B,3,dz)
-	outx = aux2-aux1						#is -z component in user coordinates...
+	outx = aux1-aux2						
 
 	aux1 = deriv_findiff(C,3,dz)       #y component of the curl
 	aux2 = deriv_findiff(A,1,dx)
-	outy = aux2-aux1
+	outy = aux1-aux2
 
 	aux1 = deriv_findiff(B,1,dx)       #z component of the curl
 	aux2 = deriv_findiff(C,2,dy)
-	outz = aux2-aux1
+	outz = aux1-aux2
 # now reverse order of results to go back to user coordinates
 	return outz, outy, outx
 
