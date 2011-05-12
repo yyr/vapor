4a9a99f068b28e318d62379b405911a1e09fbab6
southwic <>
2011-05-12 17:09:51 +0000

    *** empty log message ***

--- apps/asciitf2vtf/asciitf2vtf.cpp
+++ apps/asciitf2vtf/asciitf2vtf.cpp
@@ -4,6 +4,8 @@
 #include <vector>
 #include <sstream>
 #include <algorithm>
+#include <string>
+#include <iomanip>
 #include <vapor/CFuncs.h>
 #include <vapor/OptionParser.h>
 #include <vapor/Metadata.h>
@@ -15,10 +17,19 @@
 using namespace VetsUtil;
 using namespace VAPoR;
 
+//
+// asciitf2vtf:
+//    This is one of the commandline applications for VAPOR.
+//
+// Modified in April, 2011.
+//    Added the ability for the program to use NCL colormaps as input.
+//	Kendall Southwick
+//
 
 struct opt_t {
 	char *omap;
 	char *cmap;
+	char *type;
 	OptionParser::Boolean_T	help;
 	OptionParser::Boolean_T	quiet;
 } opt;
@@ -26,6 +37,7 @@ struct opt_t {
 OptionParser::OptDescRec_T	set_opts[] = {
 	{"cmap",	1,	"",				"Path to ascii file containing color map"},
 	{"omap",	1,	"",				"Path to ascii file containing opacity map"},
+	{"type",	1,	"vapor",		"Type pf color maps being used, defaults to vapor"},
 	{"help",	0,	"",				"Print this message and exit"},
 	{"quiet",	0,	"",				"Operate quietly"},
 	{NULL}
@@ -35,11 +47,11 @@ OptionParser::OptDescRec_T	set_opts[] = {
 OptionParser::Option_T	get_options[] = {
 	{"cmap", VetsUtil::CvtToString, &opt.cmap, sizeof(opt.cmap)},
 	{"omap", VetsUtil::CvtToString, &opt.omap, sizeof(opt.omap)},
+	{"type", VetsUtil::CvtToString, &opt.type, sizeof(opt.type)},
 	{"help", VetsUtil::CvtToBoolean, &opt.help, sizeof(opt.help)},
 	{"quiet", VetsUtil::CvtToBoolean, &opt.quiet, sizeof(opt.quiet)},
 	{NULL}
 };
-
 
 const char *ProgName;
 
@@ -56,6 +68,264 @@ void Usage(OptionParser &op, const char * msg) {
 void ErrMsgCBHandler(const char *msg, int) {
 	cerr << ProgName << " : " << msg << endl;
 }
+
+//
+// This function will process the VAPOR color map files.
+//
+
+int ProcessVAPORCMAP(TransferFunctionLite *transFunct) {
+	int RetVal = 0;
+	vector <float> pvec, hvec, svec, vvec;
+	float point, h, s, v;
+
+	ColorMapBase *cmap = transFunct->getColormap();
+	cmap->clear();
+
+	FILE *fp = fopen(opt.cmap, "r");
+	if (! fp) {
+		MyBase::SetErrMsg("fopen(%s) : %M", opt.cmap);
+		exit(1);
+	}
+
+	const char *format = "%f %f %f %f";
+
+	while ((RetVal = fscanf(fp, format, &point, &h, &s, &v)) == 4) { 
+		pvec.push_back(point);
+		hvec.push_back(h);
+		svec.push_back(s);
+		vvec.push_back(v);
+	}
+
+	if(vvec.size() <= 0) {
+		MyBase::SetErrMsg("Error parsing file %s , no data.", opt.cmap);
+		exit(1);
+	}
+
+	vector <float> tmpvec = pvec;
+	sort(tmpvec.begin(), tmpvec.end());
+
+	// Apparently we need to set the min and max values in both
+	// the transfer function and colormap class. Setting the 
+	// cmap bounds is needed so that addControlPointAt() will
+	// correctly normalize the data value.
+
+	transFunct->setMinColorMapValue(tmpvec[0]);
+	transFunct->setMaxColorMapValue(tmpvec[tmpvec.size()-1]);
+	cmap->minValue(tmpvec[0]);
+	cmap->maxValue(tmpvec[tmpvec.size()-1]);
+
+	ColorMapBase::Color color;
+	for (int i=0; i<pvec.size(); i++) {
+		color.hue(hvec[i]);
+		color.sat(svec[i]);
+		color.val(vvec[i]);
+	//	cmap->addNormControlPoint(pvec[i],  color);
+		cmap->addControlPointAt(pvec[i],  color);
+	}
+	//
+	// Ugh. now we need to normalize the data bounds.
+	//
+	cmap->minValue(0.0);
+	cmap->maxValue(1.0);
+
+	if (ferror(fp)) {
+		MyBase::SetErrMsg("Error parsing file %s", opt.cmap);
+		exit(1);
+	}
+	fclose(fp);
+	
+	return(RetVal);
+} // End of ProcessVAPORCMAP.
+
+//
+// This function will process the NCL color map files.
+//
+
+int ProcessNCLCMAP(TransferFunctionLite *transFunct) {
+	int RetVal = 0;
+	int numColors = 0;
+	int fileSize;
+	int rc;
+	int last_i;
+	float THRESHOLD = 0.10;
+	float SLOPE_THRESHOLD = 0.010;
+	float last_hsv[3];
+	float rgb[3], hsv[3], comp_vals[3];
+	vector <float> pvec, hvec, svec, vvec;
+	char *readLine;
+	bool header_flag = false;
+	bool h_slope_flag, slope_flag, interval_flag, delta_flag;
+
+	ColorMapBase *cmap = transFunct->getColormap();
+	cmap->clear();
+
+	ifstream nclFile (opt.cmap, ios::in);
+	if (! nclFile) {
+		MyBase::SetErrMsg("Unable to open file %s: %M",opt.cmap);
+		exit(1);
+	}
+
+	//
+	// Start parsing the file.
+	// There needs to be a line must be of the form
+	// "ncolors=##"  where ## is a number.
+	// The header ends with a lines of the form
+	// # r    b   g  
+	// There may a different number of spaces between the 
+	// # and the r.
+	//
+	
+	nclFile.seekg(0,ios::end);
+	fileSize = nclFile.tellg();
+	nclFile.seekg(0,ios::beg);
+	readLine = (char*)malloc(fileSize*sizeof(char));
+	char *indexChar;
+
+	
+	while(!header_flag && ! nclFile.eof()) {
+		nclFile.getline(readLine,fileSize);
+		indexChar = NULL;
+		if (readLine[0] == '#') {
+			indexChar = strstr(readLine,"r   g   b");
+			if (indexChar != NULL) {
+				header_flag = true;
+			} // end of headers.
+		} // comment line.
+		else {
+			indexChar = strstr(readLine,"ncolors=");
+			if (indexChar != NULL) {
+				rc = sscanf(indexChar+8, "%d", &numColors);
+			} // have ncolors.
+		} // non comment line.
+	} // End of while.
+	
+	if(nclFile.eof() || numColors == 0 || !header_flag) {
+		MyBase::SetErrMsg("Formating error in file %s", opt.cmap);
+		exit(1);
+	}
+	
+	//
+	// Since it is hard to have more the ten color control points
+	// in VAPOR, we need to trim the input down.
+	//
+
+	for(int i = 0; ((i < numColors) && !nclFile.eof()); i++) {
+		nclFile.getline(readLine,fileSize);
+		if (strlen(readLine) > 0) {
+			rc = sscanf(readLine, "%f %f %f", &rgb[0], &rgb[1], &rgb[2]);
+			if (rc != 3) {
+				MyBase::SetErrMsg("Formating error in file %s",opt.cmap);
+				exit(1);
+			}
+			h_slope_flag =  slope_flag = interval_flag = delta_flag = false;
+			rgb[0] = rgb[0]/ 255.0;
+			rgb[1] = rgb[1]/ 255.0;
+			rgb[2] = rgb[2]/ 255.0;
+			transFunct->rgbToHsv(rgb,hsv);
+			if(i == 0) {
+				comp_vals[0] = hsv[0];
+				comp_vals[1] = hsv[1];
+				comp_vals[2] = hsv[2];
+				last_hsv[0] = hsv[0];
+				last_hsv[1] = hsv[1];
+				last_hsv[2] = hsv[2];
+				last_i = -1;
+			}
+
+			if( (abs(hsv[0] - comp_vals[0]) >= THRESHOLD) ||
+					(abs(hsv[1] - comp_vals[1]) >= THRESHOLD) ||
+			 		(abs(hsv[2] - comp_vals[2]) >= THRESHOLD)) {
+			   delta_flag = true;
+			}
+			if ((abs((comp_vals[2]-hsv[2])/(i -last_i) - (last_hsv[2] - hsv[2])) >= SLOPE_THRESHOLD) ||
+				(abs((comp_vals[1]-hsv[1])/(i -last_i) - (last_hsv[1] - hsv[1])) >= SLOPE_THRESHOLD)) {
+				slope_flag = true;
+			}
+			if ((i - last_i ) > (0.07 * numColors)) {
+				interval_flag = true;
+			}
+			
+			if(slope_flag || interval_flag || (i==0) ) {
+			 	comp_vals[0] = hsv[0];
+				comp_vals[1] = hsv[1];
+				comp_vals[2] = hsv[2];
+				last_i = i;
+				pvec.push_back(((float)(i))/((float)(numColors)));
+				hvec.push_back(hsv[0]);
+				svec.push_back(hsv[1]);
+				vvec.push_back(hsv[2]);
+			 }
+			 last_hsv[0] = hsv[0];
+			 last_hsv[1] = hsv[1];
+			 last_hsv[2] = hsv[2];
+		} // End if strlen.
+	} // End for.
+
+	if(vvec.size() <= 0) {
+		MyBase::SetErrMsg("Error parsing file %s , no data.", opt.cmap);
+		exit(1);
+	}
+
+	vector <float> tmpvec = pvec;
+	sort(tmpvec.begin(), tmpvec.end());
+
+	// Apparently we need to set the min and max values in both
+	// the transfer function and colormap class. Setting the 
+	// cmap bounds is needed so that addControlPointAt() will
+	// correctly normalize the data value.
+
+	transFunct->setMinColorMapValue(tmpvec[0]);
+	transFunct->setMaxColorMapValue(tmpvec[tmpvec.size()-1]);
+	cmap->minValue(tmpvec[0]);
+	cmap->maxValue(tmpvec[tmpvec.size()-1]);
+
+	ColorMapBase::Color color;
+	for (int i=0; i<pvec.size(); i++) {
+		color.hue(hvec[i]);
+		color.sat(svec[i]);
+		color.val(vvec[i]);
+	//	cmap->addNormControlPoint(pvec[i],  color);
+		cmap->addControlPointAt(pvec[i],  color);
+	}
+
+	//
+	// Ugh. now we need to normalize the data bounds.
+	//
+	cmap->minValue(0.0);
+	cmap->maxValue(1.0);
+
+	nclFile.close();
+	
+	return(RetVal);
+} // End of ProcessNCLCMAP.
+
+//
+// This functions directs how the color maps is t be processed based on the 
+// type of color maps, form the -type option.
+//
+
+int ProcessCMAP(TransferFunctionLite *transFunct, char *cmapType) {
+	int RetVal;
+	
+	if(strcmp(cmapType, "vapor") == 0) {
+		RetVal = ProcessVAPORCMAP(transFunct);
+	}
+	else if(strcmp(cmapType, "VAPOR") == 0) {
+		RetVal = ProcessVAPORCMAP(transFunct);
+	}
+	else if(strcmp(cmapType, "ncl") == 0) {
+		RetVal = ProcessNCLCMAP(transFunct);
+	}
+	else if(strcmp(cmapType, "NCL") == 0) {
+		RetVal = ProcessNCLCMAP(transFunct);
+	}
+	else {		
+		RetVal = 1;
+		MyBase::SetErrMsg("Invalid color map type.");
+	}
+	
+	return (RetVal);
+} // End of ProcessCMAP.
 
 int	main(int argc, char **argv) {
 
@@ -74,7 +344,6 @@ int	main(int argc, char **argv) {
 	}
 
 	MyBase::SetErrMsgCB(ErrMsgCBHandler);
-
 
 	if (opt.help) {
 		Usage(op, NULL);
@@ -101,66 +370,10 @@ int	main(int argc, char **argv) {
 
 	string vtffile(argv[1]);
 
-
 	int rc;
 
-
 	if (strlen(opt.cmap) != 0) {
-		vector <float> pvec, hvec, svec, vvec;
-		float point, h, s, v;
-
-		ColorMapBase *cmap = tf.getColormap();
-		cmap->clear();
-
-		FILE *fp = fopen(opt.cmap, "r");
-		if (! fp) {
-			MyBase::SetErrMsg("fopen(%s) : %M", opt.cmap);
-			exit(1);
-		}
-
-		const char *format = "%f %f %f %f";
-
-		while ((rc = fscanf(fp, format, &point, &h, &s, &v)) == 4) { 
-			pvec.push_back(point);
-			hvec.push_back(h);
-			svec.push_back(s);
-			vvec.push_back(v);
-		}
-
-
-		vector <float> tmpvec = pvec;
-		sort(tmpvec.begin(), tmpvec.end());
-
-		// Apparently we need to set the min and max values in both
-		// the transfer function and colormap class. Setting the 
-		// cmap bounds is needed so that addControlPointAt() will
-		// correctly normalize the data value.
-		//
-		tf.setMinColorMapValue(tmpvec[0]);
-		tf.setMaxColorMapValue(tmpvec[tmpvec.size()-1]);
-		cmap->minValue(tmpvec[0]);
-		cmap->maxValue(tmpvec[tmpvec.size()-1]);
-
-		ColorMapBase::Color color;
-		for (int i=0; i<pvec.size(); i++) {
-			color.hue(hvec[i]);
-			color.sat(svec[i]);
-			color.val(vvec[i]);
-//			cmap->addNormControlPoint(pvec[i],  color);
-			cmap->addControlPointAt(pvec[i],  color);
-		}
-
-		//
-		// Ugh. now we need to normalize the data bounds.
-		//
-		cmap->minValue(0.0);
-		cmap->maxValue(1.0);
-
-		if (ferror(fp)) {
-			MyBase::SetErrMsg("Error parsing file %s", opt.cmap);
-			exit(1);
-		}
-		fclose(fp);
+		ProcessCMAP(&tf, opt.type);
 	}
 
 	if (strlen(opt.omap) != 0) {
@@ -202,8 +415,12 @@ int	main(int argc, char **argv) {
 			exit(1);
 		}
 		fclose(fp);
-	}
+	} // End of if omap.
 
+	//
+	// Write output file.
+	//
+	
 	ofstream fileout;
 	fileout.open(vtffile.c_str());
 	if (! fileout) {
@@ -211,9 +428,9 @@ int	main(int argc, char **argv) {
 			"Can't open file \"%s\" for writing", vtffile.c_str()
 		);
 		exit(1);
-	}
-
-	if (! (tf.saveToFile(fileout))) exit(1);
+	} 
+	if (! (tf.saveToFile(fileout)))
+		exit(1);
 
 	exit(0);
-}
+} // End of Main.
