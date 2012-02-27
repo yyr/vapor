8079f527de38868661348620981d1ebb109728e0
ypolius <>
2012-02-27 16:53:08 +0000

    Modified Files:  	Makefile.am  Added Files:  	vdfbridge.cpp 	replacement for vdc2/vdf_info, uses autoconf calls to correctly mangle fortran/c++ cross calls  Removed Files:  	vdc2.cpp vdf_info.cpp

--- lib/vdf/vdfbridge.cpp
+++ lib/vdf/vdfbridge.cpp
@@ -1,20 +1,127 @@
 #include <iostream>
-#include <cstring>
-#include <cstdio>
+#include <fstream>
+#include <string.h>
 #include <vector>
 #include <sstream>
-#include <map>
-#include <algorithm>
-#include <mpi.h>
-#include <cerrno>
-#include <vapor/CFuncs.h>
+
 #include <vapor/MetadataVDC.h>
+#include <vapor/CFuncs.h>
 #include <vapor/WaveCodecIO.h>
 
+using namespace std;
 using namespace VetsUtil;
 using namespace VAPoR;
+MetadataVDC *file;
+vector<size_t> cratios;
+vector<string> variables;
+size_t global[3];
+size_t bsize[3];
+char *wname = "bior3.3";
+char *wmode = "symh";
+string *file_path;
+#define CREATE_F90 FC_FUNC(CreateVDF)
+#define DEFINE_F90 FC_FUNC(DefVDFVar)
+#define ENDDEF_F90 FC_FUNC(EndVDFDef)
+#define WRITE_F90 FC_FUNC(WriteVDC2Var)
+#define READ_F90 FC_FUNC(ReadVDC2Var)
+extern "C" void CREATE_F90(int *griddims, int *bs, char * path, int path_len)
+{
+  cratios.push_back(1);
+  cratios.push_back(10);
+  cratios.push_back(100);
+  cratios.push_back(500);
+  global[0] = griddims[0];
+  global[1] = griddims[1];
+  global[2] = griddims[2];
+  bsize[0] = bs[0];
+  bsize[1] = bs[1];
+  bsize[2] = bs[2];
+  file_path = new string(path);
+  file = new MetadataVDC(global, bsize, cratios, wname, wmode);
+}
+extern "C" void DEFINE_F90(char * name, int name_len)
+{
+  string var(name);
+  variables.push_back(name);
+  int pos = std::find(variables.begin(), variables.end(), name);
+  *varid = pos;
+}
+extern "C" void ENDDEF_F90()
+{
+  file->SetVariables3D(variables);
+  file->write(file_path);
+  delete file;
+}
+extern "C" void WRITE_F90(float *array, int *start, int *len, int *IO_Comm_f, int *ts, int *lod, int *reflevel, char *name, int name_len, int num_iotasks) //AIX no name mangling
+{
+  
+  int rank = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-void xform_and_write(const float *data, int *start, int *end, int *count,MPI_Comm IOComm, int *ts, int *lod, int *global, char* vdf, int* bsize,  char* name, int num_iotasks, int reflevel){
+  MPI_Comm IO_Comm = MPI_Comm_f2c(*IO_Comm_f);
+#ifdef PIOVDC_DEBUG
+  std::cout << "@example data write_vdc2 " << array[0] << ":" << array[1] << std::endl;
+#endif
+  MyBase::SetErrMsgFilePtr(stderr);
+
+  MyBase::SetDiagMsg("@ndims: %d array[0]: %f\n", ndims, array[0]);
+  for(int i=0; i<*ndims; i++)
+    MyBase::SetDiagMsg("@start[%d]: %d, len[%d]: %d\n", i, start[i], i, len[i]);
+  int start_block[3];
+  int end_block[3];
+
+  end_block[0] = start[0] + len[0] - 2 ;
+
+  end_block[1] = start[1] + len[1] - 2;
+
+  end_block[2] = start[2] + len[2] - 2;
+
+#ifdef DEBUG
+  std::cout << "VDC2 rank: " << rank << " start: " << start_block[0] << "," << start_block[1] << "," << start_block[2] << " end: " << end_block[0] << "," << end_block[1] << "," << end_block[2] << " start2: " << start[0] << "," << start[1] << "," << start[2] << " len: " << len[0] << "," << len[1] << "," << len[2] <<  " bsize: " << bsize[0] << "," << bsize[1] << "," << bsize[2] << std::endl;
+#endif
+  MyBase::SetDiagMsg("@st: %d %d %d, en: %d %d %d\n", 
+		     start_block[0], start_block[1], start_block[2],
+		     end_block[0], end_block[1], end_block[2]);
+  xform_and_write(array, start, end_block, len, IO_Comm, ts, lod, bsize, name, num_iotasks);
+
+#ifdef PIOVDC_DEBUG
+  if(rank == 0)
+    std::cout << "VDC2 transforms complete" << std::endl;
+#endif
+}
+
+extern "C" void READ_F90(float *array, int *start, int *len, int *IO_Comm_f, int *ts, int *lod, int *reflevel, char *name, int name_len, int num_iotasks) //AIX no name mangling
+{
+  int rank = 0;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  MPI_Comm IO_Comm = MPI_Comm_f2c(*IO_Comm_f);
+
+  MyBase::SetErrMsgFilePtr(stderr);
+  int start_block[3];
+  int end_block[3];
+
+  end_block[0] = start[0] + len[0] - 2 ;
+
+  end_block[1] = start[1] + len[1] - 2;
+
+  end_block[2] = start[2] + len[2] - 2;
+
+#ifdef DEBUG
+  std::cout << "VDC2 read_vdc2_var rank: " << rank << " start: " << start_block[0] << "," << start_block[1] << "," << start_block[2] << " end: " << end_block[0] << "," << end_block[1] << "," << end_block[2] << " start2: " << start[0] << "," << start[1] << "," << start[2] << " len: " << len[0] << "," << len[1] << "," << len[2] <<  " bsize: " << bsize[0] << "," << bsize[1] << "," << bsize[2] << " vdf: " << vdf << " name: " << name << std::endl;
+#endif
+  MyBase::SetDiagMsg("@st: %d %d %d, en: %d %d %d\n", 
+		     start_block[0], start_block[1], start_block[2],
+		     end_block[0], end_block[1], end_block[2]);
+  inverse_and_read(array, start, end_block, len, IO_Comm, ts, lod, reflevel, bsize, name);
+
+#ifdef DEBUG
+  if(rank == 0)
+    std::cout << "VDC2 transforms complete" << std::endl;
+#endif
+}
+
+void xform_and_write(const float *data, int *start, int *end, int *count,MPI_Comm IOComm, int *ts, int *lod, int *reflevel,  char* name, int num_iotasks){
   //Obtain MPI Rank for timing
 
   int rank = 0;
@@ -22,7 +129,7 @@ void xform_and_write(const float *data, int *start, int *end, int *count,MPI_Com
 #ifdef PIOVDC_DEBUG
   double starttime = MPI_Wtime();
 #endif
-  MetadataVDC metadata(vdf);
+  MetadataVDC metadata(file_path.c_str());
   WaveCodecIO *wcwriter = new WaveCodecIO(metadata, 1);
 
   wcwriter->SetIOComm(IOComm);
@@ -47,16 +154,13 @@ void xform_and_write(const float *data, int *start, int *end, int *count,MPI_Com
   size_t temp_count[3] = {count[0], count[1], count[2]};
 
   wcwriter->EnableBuffering(temp_count, 1, rank);
-
-  size_t *dims = (size_t*)malloc(sizeof(size_t) * 3);
-  metadata.GetGridDim(dims);
   //Write out data array
   if (wcwriter->GetErrCode() != 0) perror("wcwriter ctor failed\n");
 #ifdef PIOVDC_DEBUG
   double openbegin = MPI_Wtime();
 #endif
 
-  if (wcwriter->OpenVariableWrite((size_t)*ts, name, reflevel, *lod) < 0) perror("openVar failed\n");
+  if (wcwriter->OpenVariableWrite((size_t)*ts, name, *reflevel, *lod) < 0) perror("openVar failed\n");
 #ifdef PIOVDC_DEBUG
   double opentime = MPI_Wtime() - openbegin;
 #endif
@@ -153,45 +257,7 @@ void xform_and_write(const float *data, int *start, int *end, int *count,MPI_Com
   delete wcwriter;
 }
 
-extern "C" void write_vdc2_var_(float *array, int *start, int *len, int *ndims, int *IO_Comm_f, int *bsize, int *ts, int *lod, int *global, char *vdf, char *name, int vdf_len, int name_len, int num_iotasks, int reflevel) //AIX no name mangling
-{
-  
-  int rank = 0;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-  MPI_Comm IO_Comm = MPI_Comm_f2c(*IO_Comm_f);
-#ifdef PIOVDC_DEBUG
-  std::cout << "@example data write_vdc2 " << array[0] << ":" << array[1] << std::endl;
-#endif
-  MyBase::SetErrMsgFilePtr(stderr);
-
-  MyBase::SetDiagMsg("@ndims: %d array[0]: %f\n", ndims, array[0]);
-  for(int i=0; i<*ndims; i++)
-    	MyBase::SetDiagMsg("@start[%d]: %d, len[%d]: %d\n", i, start[i], i, len[i]);
-  int start_block[3];
-  int end_block[3];
-
-  end_block[0] = start[0] + len[0] - 2 ;
-
-  end_block[1] = start[1] + len[1] - 2;
-
-  end_block[2] = start[2] + len[2] - 2;
-
-#ifdef PIOVDC_DEBUG
-  std::cout << "VDC2 rank: " << rank << " start: " << start_block[0] << "," << start_block[1] << "," << start_block[2] << " end: " << end_block[0] << "," << end_block[1] << "," << end_block[2] << " start2: " << start[0] << "," << start[1] << "," << start[2] << " len: " << len[0] << "," << len[1] << "," << len[2] <<  " bsize: " << bsize[0] << "," << bsize[1] << "," << bsize[2] << std::endl;
-#endif
-    MyBase::SetDiagMsg("@st: %d %d %d, en: %d %d %d\n", 
-		       start_block[0], start_block[1], start_block[2],
-		       end_block[0], end_block[1], end_block[2]);
-    xform_and_write(array, start, end_block, len, IO_Comm, ts, lod, global, vdf, bsize, name, num_iotasks, reflevel);
-
-#ifdef PIOVDC_DEBUG
-    if(rank == 0)
-      std::cout << "VDC2 transforms complete" << std::endl;
-#endif
-}
-
-void inverse_and_read(float *data, int *start, int *end, int *count,MPI_Comm IOComm, int *ts, int *lod, int *global, char* vdf, int* bsize,  char* name, int reflevel){
+void inverse_and_read(float *data, int *start, int *end, int *count,MPI_Comm IOComm, int *ts, int *lod, int reflevel, char* vdf,  char* name){
   int rank = 0;
   MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 #ifdef PIOVDC_DEBUG
@@ -212,14 +278,12 @@ void inverse_and_read(float *data, int *start, int *end, int *count,MPI_Comm IOC
   wcwriter->SetCollectiveIO(collective);
   size_t temp_count[3] = {count[0], count[1], count[2]};
 
-  size_t *dims = (size_t*)malloc(sizeof(size_t) * 3);
-  metadata.GetGridDim(dims);
   //Write out data array
   if (wcwriter->GetErrCode() != 0) perror("wcwriter ctor failed\n");
 #ifdef PIOVDC_DEBUG
   double openbegin = MPI_Wtime();
 #endif
-  if (wcwriter->OpenVariableRead((size_t)*ts, name, reflevel, *lod) < 0) perror("openVar failed\n");
+  if (wcwriter->OpenVariableRead((size_t)*ts, name, *reflevel, *lod) < 0) perror("openVar failed\n");
 #ifdef PIOVDC_DEBUG
   double opentime = MPI_Wtime() - openbegin;
 #endif
@@ -266,36 +330,5 @@ void inverse_and_read(float *data, int *start, int *end, int *count,MPI_Comm IOC
   std::cout << "rank: " << rank << " VDC2 VDF aggregate time: " << vdftime  << " xformMPI: " << wcwriter->xformMPI << " ioMPI: " << wcwriter->ioMPI << " BlockWriteRegionTimer: " << wcwriter->methodTimer << " BlockWriteRegionThreadTimer: " << wcwriter->methodThreadTimer <<  " OpenVarReadTime: " << opentime << " CloseVarReadTime: " << closetime << std::endl;
 #endif
   delete wcwriter;
-}
-
-extern "C" void read_vdc2_var_(float *array, int *start, int *len, int *ndims, int *IO_Comm_f, int *bsize, int *ts, int *lod, int *global, char *vdf, char *name, int vdf_len, int name_len, int reflevel) {
-  
-  int rank = 0;
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-
-  MPI_Comm IO_Comm = MPI_Comm_f2c(*IO_Comm_f);
-
-  MyBase::SetErrMsgFilePtr(stderr);
-  int start_block[3];
-  int end_block[3];
-
-  end_block[0] = start[0] + len[0] - 2 ;
-
-  end_block[1] = start[1] + len[1] - 2;
-
-  end_block[2] = start[2] + len[2] - 2;
-
-#ifdef PIOVDC_DEBUG
-  std::cout << "VDC2 read_vdc2_var rank: " << rank << " start: " << start_block[0] << "," << start_block[1] << "," << start_block[2] << " end: " << end_block[0] << "," << end_block[1] << "," << end_block[2] << " start2: " << start[0] << "," << start[1] << "," << start[2] << " len: " << len[0] << "," << len[1] << "," << len[2] <<  " bsize: " << bsize[0] << "," << bsize[1] << "," << bsize[2] << " vdf: " << vdf << " name: " << name << std::endl;
-#endif
-    MyBase::SetDiagMsg("@st: %d %d %d, en: %d %d %d\n", 
-		       start_block[0], start_block[1], start_block[2],
-		       end_block[0], end_block[1], end_block[2]);
-    inverse_and_read(array, start, end_block, len, IO_Comm, ts, lod, global, vdf, bsize, name, reflevel);
-
-#ifdef PIOVDC_DEBUG
-    if(rank == 0)
-      std::cout << "VDC2 transforms complete" << std::endl;
-#endif
 }
 
