875e5e74ac490719223bb99ed7ad0df5dae8b68c
clynejp <>
2013-05-21 22:47:38 +0000

    Fixed regression - default variable "var1" not created

--- apps/vdfcreate/vdfcreate.cpp
+++ apps/vdfcreate/vdfcreate.cpp
@@ -81,6 +81,11 @@ int	main(int argc, char **argv) {
 	size_t dims[] = {opt.dim.nx, opt.dim.ny, opt.dim.nz};
 	file = vdcf.New(dims);
 
+	if (file->GetVariableNames().size() == 0) {
+		vector <string> vars(1, "var1");
+		file->SetVariables3D(vars);
+	}
+
 	if (! file) exit(1);
 
 
