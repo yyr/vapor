add5b6d6e633e5f2d3ea95c896558f6c3f0669d9
John Clyne <clyne@ucar.edu>
2013-12-04 14:30:31 -0700

    Workaround for missing 10.6 SDK support under Mac OSX 10.8

--- make/config/Darwin.mk
+++ make/config/Darwin.mk
@@ -7,18 +7,30 @@ QT_FRAMEWORK = 1
 endif
 
 CXXFLAGS          += -DDARWIN -Wall -Wno-format -Wno-sign-compare  -fPIC  
+
+ifeq ($(BUILD_FOR_MACOSX10_6), 1)
 CXXFLAGS          += -mmacosx-version-min=10.6 -isysroot /Developer/SDKs/MacOSX10.6.sdk
+endif
+
 CXXFLAGS          += -Wno-overloaded-virtual
 CXX_RELEASE_FLAGS += -O3 -DNDEBUG
 CXX_DEBUG_FLAGS   += -g
 
 CFLAGS            += -DDARWIN -Wall -Wno-format -fPIC
+
+ifeq ($(BUILD_FOR_MACOSX10_6), 1)
 CFLAGS            += -mmacosx-version-min=10.6 -isysroot /Developer/SDKs/MacOSX10.6.sdk
+endif
+
 C_RELEASE_FLAGS   += -O3 -DNDEBUG
 C_DEBUG_FLAGS     += -g
 
 LDFLAGS           += -framework CoreFoundation
+
+ifeq ($(BUILD_FOR_MACOSX10_6), 1)
 LDFLAGS           += -mmacosx-version-min=10.6
+endif
+
 LD_RELEASE_FLAGS  += 
 LD_DEBUG_FLAGS    += 
 
@@ -26,13 +38,21 @@ PROFILEFLAGS = -pg -a
 
 ifeq ($(MACHTYPE),x86_64)
 SHARED_LDFLAGS += -m64 
+
+ifeq ($(BUILD_FOR_MACOSX10_6), 1)
 SHARED_LDFLAGS += -mmacosx-version-min=10.6
+endif 
+
 LDFLAGS += -m64
 CXXFLAGS += -m64
 CFLAGS += -m64
 else 
 SHARED_LDFLAGS += -m32 
+
+ifeq ($(BUILD_FOR_MACOSX10_6), 1)
 SHARED_LDFLAGS += -mmacosx-version-min=10.6
+endif
+
 LDFLAGS += -m32
 CXXFLAGS += -m32
 CFLAGS += -m32
