6c77f33c3d73f22260e24d2a50048d93eaa7ad8d
Scott Pearse <pearse@cisl-ebern.local>
2013-12-09 09:42:49 -0700

    Test fix for bug #935, "VDCWizard Windows 8 display problem"

--- apps/vdcwizard/intropage.cpp
+++ apps/vdcwizard/intropage.cpp
@@ -71,5 +71,10 @@ void IntroPage::on_populateDataButton_clicked()
 
 void IntroPage::initializePage() {
 	QList<QWizard::WizardButton> layout;
-    wizard()->setButtonLayout(layout);
+    layout << QWizard::NextButton;
+	wizard()->setButtonLayout(layout);
+	
+	QList<QWizard::WizardButton> overwriteLayout;
+	wizard()->setButtonLayout(overwriteLayout);
+	//wizard()->button(QWizard::NextButton)->hide();
 }
