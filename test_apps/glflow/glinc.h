9d4f4900063a1e0ac09c09ca4807b4a463dc81f5
John Clyne <clyne@ucar.edu>
2014-03-28 16:59:41 -0600

    Ported to Mac

--- test_apps/glflow/glinc.h
+++ test_apps/glflow/glinc.h
@@ -2,9 +2,15 @@
 #define GLINC_H
 
 //#include <GL/glew.h>
+#ifdef	Darwin
+#include <OpenGL/gl.h>
+#include <OpenGL/glu.h>
+#include <glut.h>
+#else
 #include <GL/gl.h>
 #include <GL/glu.h>
 #include <GL/glut.h>
+#endif
 
 #endif
 
