822593fefb2a7f4fdc25db18427931c5c0b07634
Alan Norton <alan@ucar.edu>
2014-08-13 12:27:44 -0600

    Updated doxygen for ArrowRenderer

--- lib/render/arrowrenderer.h
+++ lib/render/arrowrenderer.h
@@ -25,36 +25,64 @@
 
 namespace VAPoR {
 
+//! \class ArrowRenderer
+//! \brief Class that draws the barbs as specified by ArrowParams
+//! \author Alan Norton
+//! \version 3.0
+//! \date February 2014
+
   class RENDER_API ArrowRenderer : public Renderer
   {
 	
       
   public:
-	
+//! Constructor, must invoke Renderer constructor
+//! \param[in] Visualizer* pointer to the visualizer where this will draw
+//! \param[in] RenderParams* pointer to the ArrowParams describing this renderer
     ArrowRenderer(Visualizer *w, RenderParams* rp);
+
+//! Destructor
     virtual ~ArrowRenderer();
-	virtual void initializeGL();
-	virtual void paintGL();
-	virtual void setAllDataDirty(){} //No need to do anything.
+
+//! Virtual method performs any OpenGL initialization, sets the initialized flag
+    virtual void initializeGL();
+
+//! Virtual method issues all the OpenGL calls to draw the arrows in user coordinates. 
+    virtual void paintGL();
+
+//! Virtual method to force rebuilding of any caches associated with this renderer.
+//! The ArrowRender has no caches so this method does nothing.
+    virtual void setAllDataDirty(){} //No need to do anything.
 
   protected:
-	//Method that gets the required data from the DataMgr, while determining valid extents.
-	//Returns the available refinement level, or -1 if needed data not available.
+
+//! Protected method that gets the required data from the DataMgr, while determining valid extents.
+//! \param[in] vector<string>& varnames Names of variables defining field
+//! \param[out] RegularGrid* RegularGrids obtained from the DataMgr
+//! \param[out] double validExts[6] Extents of valid data 
+//! \param[out] size_t voxExts[6] Voxel extents of valid data
+//! \return int refinement level of data, or -1 if unavailable
     int setupVariableData(
 		vector<string>& varnames, RegularGrid *varData[4], double validExts[6], 
 		size_t voxExts[6]
 	);
 
-	//Perform the OpenGL rendering
+//! Protected method that performs rendering of all arrows.
+//! \param[in] int actualRefLevel refinement level to be rendered.
+//! \param[in] float vectorScale Scale factor to be applied to arrows.
+//! \param[in] float arrowRadius Radius of arrows in voxel diameters.
+//! \param[in] RegularGrid*[4] RegularGrids used in rendering
 	void performRendering(
 		int actualRefLevel,float vectorScale, float arrowRadius, 
 		RegularGrid *variableData[4]
 	);
 
-	//Draw one arrow
+//! Protected method to draw one arrow (a hexagonal tube with a cone arrowhead)
+//! \param[in] const float startPoint[3] beginning position of arrow
+//! \param[in] const float endPoint[3] ending position of arrow
+//! \param[in] float radius Radius of arrow in voxels
 	void drawArrow(const float startPoint[3], const float endPoint[3], float radius);
 
-  private:
 
 
   };
