ee2c7945e970ca4cd743d1b21a0f460261cdc49e
John Clyne <clyne@ucar.edu>
2015-01-20 16:50:07 -0700

    intermediate commit - prototype WRF conversion command line utilities

---
+++ apps/wrf2vdc/wrf2vdc.cpp
@@ -0,0 +1,275 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <vector>
+#include <sstream>
+
+#include <vapor/OptionParser.h>
+#include <vapor/CFuncs.h>
+#include <vapor/VDCNetCDF.h>
+#include <vapor/DCWRF.h>
+
+using namespace VetsUtil;
+using namespace VAPoR;
+
+
+struct opt_t {
+	int nthreads;
+    std::vector <string> vars;
+	OptionParser::Boolean_T	help;
+} opt;
+
+OptionParser::OptDescRec_T	set_opts[] = {
+	{
+		"nthreads",    1,  "0",    "Specify number of execution threads "
+		"0 => use number of cores"
+	},
+	{
+		"vars",1, "",
+		"Colon delimited list of 3D variable names (compressed) "
+		"to be included in "
+		"the VDC"
+	},
+	{"help",	0,	"",	"Print this message and exit"},
+	{NULL}
+};
+
+
+OptionParser::Option_T	get_options[] = {
+	{"nthreads", VetsUtil::CvtToInt, &opt.nthreads, sizeof(opt.nthreads)},
+	{"vars", VetsUtil::CvtToStrVec, &opt.vars, sizeof(opt.vars)},
+	{"help", VetsUtil::CvtToBoolean, &opt.help, sizeof(opt.help)},
+	{NULL}
+};
+
+string ProgName;
+
+SmartBuf dataBuffer;
+
+// Copy a variable with 2 or 3 spatial dimensions
+//
+int CopyVar2d3d(
+	DC &dc, VDC &vdc, const vector <DC::Dimension> &dims,
+	size_t ts, string varname, int lod
+) {
+
+	vector <DC::Dimension> mydims = dims;
+	// See if time varying - time dimensions are always the slowest varying
+	//
+	if (mydims[mydims.size()-1].GetAxis() == 3) {
+		mydims.pop_back();
+	}
+	assert(mydims.size()==2 || mydims.size() == 3);
+	
+	int rc = dc.OpenVariableRead(ts,varname, -1, -1);
+	if (rc<0) {
+		MyBase::SetErrMsg(
+			"Failed to open variable %s for reading\n", varname.c_str()
+		);
+		return(-1);
+	}
+
+    rc = vdc.OpenVariableWrite(ts, varname, lod);
+    if (rc<0) {
+		MyBase::SetErrMsg(
+			"Failed to open variable %s for writing\n", varname.c_str()
+		);
+		return(-1);
+	}
+
+    size_t nz = mydims.size() > 2 ? mydims[mydims.size()-1].GetLength() : 1;
+
+	size_t sz = mydims[0].GetLength() * mydims[1].GetLength();
+	float *buf = (float *) dataBuffer.Alloc(sz);
+
+    for (size_t i=0; i<nz; i++) {
+        rc = dc.ReadSlice(buf);
+		if (rc<0) {
+			MyBase::SetErrMsg(
+				"Failed to read variable %s\n", varname.c_str()
+			);
+			return(-1);
+		}
+
+        int rc = vdc.WriteSlice(buf);
+		if (rc<0) {
+			MyBase::SetErrMsg(
+				"Failed to write variable %s\n", varname.c_str()
+			);
+			return(-1);
+		}
+	}
+
+    (void) dc.CloseVariable();
+    rc = vdc.CloseVariable();
+	if (rc<0) {
+		MyBase::SetErrMsg(
+			"Failed to write variable %s\n", varname.c_str()
+		);
+		return(-1);
+	}
+
+	return(0);
+}
+
+// Copy a variable with 0 or 1 spatial dimensions
+//
+int CopyVar0d1d(
+	DC &dc, VDC &vdc, const vector <DC::Dimension> &dims,
+	size_t ts, string varname, int lod
+) {
+
+	vector <DC::Dimension> mydims = dims;
+
+	// See if time varying - time dimensions are always the slowest varying
+	//
+	if (mydims[mydims.size()-1].GetAxis() == 3) {
+		mydims.pop_back();
+	}
+	assert(mydims.size()==0 || mydims.size() == 1);
+
+	size_t sz=1;
+	for (int i=0; i<mydims.size(); i++) {
+		sz *= mydims[i].GetLength();
+	}
+
+	float *buf = (float *) dataBuffer.Alloc(sz);
+
+	int rc = dc.GetVar(ts, varname, -1, -1, buf);
+	if (rc<0) {
+		MyBase::SetErrMsg(
+			"Failed to read variable %s\n", varname.c_str()
+		);
+		return(-1);
+	}
+
+	rc = vdc.PutVar(ts, varname, lod, buf);
+	if (rc<0) {
+		MyBase::SetErrMsg(
+			"Failed to write variable %s\n", varname.c_str()
+		);
+		return(-1);
+	}
+
+	return(0);
+}
+
+
+int CopyVar(
+	DC &dc, VDC &vdc,
+	size_t ts, string varname, int lod
+) {
+
+	DC::BaseVar var;
+	bool ok = vdc.GetBaseVarInfo(varname, var);
+	assert(ok==true);
+
+	vector <DC::Dimension> dims = var.GetDimensions();
+	assert(dims.size() > 0 && dims.size() < 5);
+
+	// Call appropriate copy method based on number of spatial dimensions
+	//
+	if (dims.size() == 1 || (dims.size()==2 && dims[1].GetAxis() == 3)) {
+		return(CopyVar0d1d(dc, vdc, dims, ts, varname, lod));
+	}
+	else {
+		return(CopyVar2d3d(dc, vdc, dims, ts, varname, lod));
+	}
+}
+	
+
+int	main(int argc, char **argv) {
+
+	OptionParser op;
+
+	MyBase::SetErrMsgFilePtr(stderr);
+	//
+	// Parse command line arguments
+	//
+	ProgName = Basename(argv[0]);
+
+
+	if (op.AppendOptions(set_opts) < 0) {
+		exit(1);
+	}
+
+	if (op.ParseOptions(&argc, argv, get_options) < 0) {
+		exit(1);
+	}
+
+	if (argc < 3) {
+		cerr << "Usage: " << ProgName << "wrffiles... master.nc" << endl;
+		op.PrintOptionHelp(stderr, 80, false);
+		exit(1);
+	}
+	
+
+	if (opt.help) {
+		cerr << "Usage: " << ProgName << " master.nc" << endl;
+		op.PrintOptionHelp(stderr, 80, false);
+		exit(0);
+	}
+
+	argc--;
+	argv++;
+	vector <string> wrffiles;
+	for (int i=0; i<argc-1; i++) wrffiles.push_back(argv[i]);
+	string master = argv[argc-1];
+
+	VDCNetCDF    vdc(opt.nthreads);
+
+
+	size_t chunksize = 1024*1024*4;
+	int rc = vdc.Initialize(master, VDC::A, chunksize);
+	if (rc<0) exit(1);
+
+	DCWRF	dcwrf;
+	rc = dcwrf.Initialize(wrffiles);
+	if (rc<0) {
+		exit(1);
+	}
+
+	// Necessary????
+	//
+//	rc = vdc.EndDefine();
+//	if (rc<0) {
+//		MyBase::SetErrMsg(
+//			"Failed to write VDC master file : %s", master.c_str()
+////		);
+//		exit(1);
+//	}
+
+	vector <string> varnames = dcwrf.GetCoordVarNames();
+	for (int i=0; i<varnames.size(); i++) {
+		int nts = dcwrf.GetNumTimeSteps(varnames[i]);
+		assert(nts >= 0);
+
+		cout << "Copying variable " << varnames[i] << endl;
+
+		for (int ts=0; ts<nts; ts++) {
+			cout << "  Time step " << ts << endl;
+			int rc = CopyVar(dcwrf, vdc, ts, varnames[i], -1);
+			if (rc<0) exit(1);
+		}
+	}
+
+	varnames = dcwrf.GetDataVarNames();
+	for (int i=0; i<varnames.size(); i++) {
+		int nts = dcwrf.GetNumTimeSteps(varnames[i]);
+		assert(nts >= 0);
+
+		cout << "Copying variable " << varnames[i] << endl;
+
+		for (int ts=0; ts<nts; ts++) {
+
+			cout << "  Time step " << ts << endl;
+
+			int rc = CopyVar(dcwrf, vdc, ts, varnames[i], -1);
+			if (rc<0) exit(1);
+		}
+	}
+
+
+	exit(0);
+
+}
---
+++ apps/wrfvdccreate/wrfvdccreate.cpp
@@ -0,0 +1,261 @@
+#include <iostream>
+#include <fstream>
+#include <string.h>
+#include <vector>
+#include <sstream>
+
+#include <vapor/OptionParser.h>
+#include <vapor/CFuncs.h>
+#include <vapor/VDCNetCDF.h>
+#include <vapor/DCWRF.h>
+
+using namespace VetsUtil;
+using namespace VAPoR;
+
+
+struct opt_t {
+	OptionParser::Dimension3D_T dim;
+	std::vector <size_t> bs;
+    std::vector <size_t> cratios;
+	string wname;
+	int nthreads;
+    std::vector <string> vars;
+	OptionParser::Boolean_T	force;
+	OptionParser::Boolean_T	help;
+} opt;
+
+OptionParser::OptDescRec_T	set_opts[] = {
+	{
+		"dimension",1, "512x512x512",  "Data volume dimensions expressed in "
+        "grid points (NXxNYxNZ)"
+	},
+	{
+		"bs", 1, "64:64:64",
+		"Internal storage blocking factor expressed in grid points (NX:NY:NZ)"
+	},
+	{
+		"cratios",1, "1:10:100:500", "Colon delimited list compression ratios. "
+		"for 3D variables. The default is 1:10:100:500. The maximum "
+		"compression ratio is wavelet and block size dependent."
+	},
+	{
+		"wname", 1,"bior3.3", "Wavelet family used for compression "
+		"Valid values are bior1.1, bior1.3, "
+		"bior1.5, bior2.2, bior2.4 ,bior2.6, bior2.8, bior3.1, bior3.3, "
+		"bior3.5, bior3.7, bior3.9, bior4.4"
+	},
+	{
+		"nthreads",    1,  "0",    "Specify number of execution threads "
+		"0 => use number of cores"
+	},
+	{
+		"vars",1, "",
+		"Colon delimited list of 3D variable names (compressed) "
+		"to be included in "
+		"the VDC"
+	},
+	{"force",	0,	"",	"Create a new VDC master file even if a VDC data "
+	"directory already exists. Results may be undefined if settings between "
+	"the new master file and old data directory do not match."},
+	{"help",	0,	"",	"Print this message and exit"},
+	{NULL}
+};
+
+
+OptionParser::Option_T	get_options[] = {
+	{"dimension", VetsUtil::CvtToDimension3D, &opt.dim, sizeof(opt.dim)},
+	{"bs", VetsUtil::CvtToSize_tVec, &opt.bs, sizeof(opt.bs)},
+	{"cratios", VetsUtil::CvtToSize_tVec, &opt.cratios, sizeof(opt.cratios)},
+	{"wname", VetsUtil::CvtToCPPStr, &opt.wname, sizeof(opt.wname)},
+	{"nthreads", VetsUtil::CvtToInt, &opt.nthreads, sizeof(opt.nthreads)},
+	{"vars", VetsUtil::CvtToStrVec, &opt.vars, sizeof(opt.vars)},
+	{"force", VetsUtil::CvtToBoolean, &opt.force, sizeof(opt.force)},
+	{"help", VetsUtil::CvtToBoolean, &opt.help, sizeof(opt.help)},
+	{NULL}
+};
+
+string ProgName;
+
+int	main(int argc, char **argv) {
+
+	OptionParser op;
+
+	MyBase::SetErrMsgFilePtr(stderr);
+	//
+	// Parse command line arguments
+	//
+	ProgName = Basename(argv[0]);
+
+
+	if (op.AppendOptions(set_opts) < 0) {
+		exit(1);
+	}
+
+	if (op.ParseOptions(&argc, argv, get_options) < 0) {
+		exit(1);
+	}
+
+	if (argc < 3) {
+		cerr << "Usage: " << ProgName << "wrffiles... master.nc" << endl;
+		op.PrintOptionHelp(stderr, 80, false);
+		exit(1);
+	}
+	
+
+	if (opt.help) {
+		cerr << "Usage: " << ProgName << " master.nc" << endl;
+		op.PrintOptionHelp(stderr, 80, false);
+		exit(0);
+	}
+
+	argc--;
+	argv++;
+	vector <string> wrffiles;
+	for (int i=0; i<argc-1; i++) wrffiles.push_back(argv[i]);
+	string master = argv[argc-1];
+
+	VDCNetCDF    vdc(opt.nthreads);
+
+	if (vdc.DataDirExists(master) && !opt.force) {
+		MyBase::SetErrMsg(
+			"Data directory exists and -force option not used. "
+			"Remove directory %s or use -force", vdc.GetDataDir(master).c_str()
+		);
+		exit(1);
+	}
+
+	size_t chunksize = 1024*1024*4;
+	int rc = vdc.Initialize(master, VDC::W, chunksize);
+	if (rc<0) exit(1);
+
+	DCWRF	dcwrf;
+	rc = dcwrf.Initialize(wrffiles);
+	if (rc<0) {
+		exit(1);
+	}
+
+	vector <string> dimnames = dcwrf.GetDimensionNames();
+	for (int i=0; i<dimnames.size(); i++) {
+		DC::Dimension dim;
+		dcwrf.GetDimension(dimnames[i], dim);
+		rc = vdc.DefineDimension(dim.GetName(), dim.GetLength(), dim.GetAxis());
+		if (rc<0) {
+			exit(1);
+		}
+	}
+
+	// Make the default block dimension 64 for any missing dimensions
+	//
+	vector <size_t> bs = opt.bs;
+	for (int i=bs.size(); i<3; i++) bs.push_back(64);
+
+	//
+	// Define coordinate variables
+	//
+	for (int d=0; d<4; d++) {
+		vector <string> coordnames = dcwrf.DC::GetCoordVarNames(d, true);
+
+
+		//
+		// Time coordinate and 1D coordinates are not blocked
+		//
+		vector <size_t> mybs;
+		if (d < 2) {
+			mybs.clear();
+		}
+		else {
+			mybs = opt.bs;
+		}
+
+		vector <size_t> cratios(1,1);
+
+		rc = vdc.SetCompressionBlock(mybs, "", cratios);
+		if (rc<0) exit(1);
+
+		for (int i=0; i<coordnames.size(); i++) {
+cout << d << " " << coordnames[i] << endl;
+			DC::CoordVar cvar;
+			dcwrf.GetCoordVarInfo(coordnames[i], cvar);
+
+			vector <DC::Dimension> dims = cvar.GetDimensions();
+			vector <string> dimnames;
+			for (int i=0; i<dims.size(); i++) {
+				dimnames.push_back(dims[i].GetName());
+			}
+		
+			rc = vdc.DefineCoordVar(
+				cvar.GetName(), dimnames, cvar.GetUnits(), 
+				cvar.GetAxis(), cvar.GetXType(), false
+			);
+
+			if (rc<0) {
+				exit(1);
+			}
+		}
+	}
+
+	//
+	// Define data variables
+	//
+	for (int d=0; d<4; d++) {
+		vector <string> datanames = dcwrf.DC::GetDataVarNames(d, true);
+
+		//
+		// Time coordinate and 1D coordinates are not blocked
+		//
+		vector <size_t> mybs;
+		if (d < 2) {
+			mybs.clear();
+		}
+		else {
+			mybs = opt.bs;
+		}
+
+		// Try to compute "reasonable" 1D & 2D compression ratios from 3D
+		// compression ratios
+		//
+		vector <size_t> cratios = opt.cratios;
+		for (int i=0; i<cratios.size(); i++) {
+			size_t c = (size_t) pow(
+				(double) cratios[i], (double) ((float) d / 3.0)
+			);
+			cratios[i] = c;
+		}
+
+		rc = vdc.SetCompressionBlock(mybs, "", cratios);
+		if (rc<0) exit(1);
+
+		for (int i=0; i<datanames.size(); i++) {
+			DC::DataVar dvar;
+			dcwrf.GetDataVarInfo(datanames[i], dvar);
+
+			vector <DC::Dimension> dims = dvar.GetDimensions();
+			vector <string> dimnames;
+			for (int i=0; i<dims.size(); i++) {
+				dimnames.push_back(dims[i].GetName());
+			}
+
+			vector <string> coordvars = dvar.GetCoordvars();
+		
+			rc = vdc.DefineDataVar(
+				dvar.GetName(), dimnames, coordvars, dvar.GetUnits(), 
+				dvar.GetXType(), true
+			);
+
+			if (rc<0) {
+				exit(1);
+			}
+		}
+	}
+	
+	rc = vdc.EndDefine();
+	if (rc<0) {
+		MyBase::SetErrMsg(
+			"Failed to write VDC master file : %s", master.c_str()
+		);
+		exit(1);
+	}
+
+	exit(0);
+
+}
