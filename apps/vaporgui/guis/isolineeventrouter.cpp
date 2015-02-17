910a28a8d5e374624d7258563e7fd7d7d8286103
Alan Norton <alan@ucar.edu>
2015-02-17 14:00:22 -0700

    Fix for 1112

--- apps/vaporgui/guis/isolineeventrouter.cpp
+++ apps/vaporgui/guis/isolineeventrouter.cpp
@@ -561,6 +561,8 @@ void IsolineEventRouter::confirmText(bool /*render*/){
 	IsolineParams* isolineParams = VizWinMgr::getActiveIsolineParams();
 	PanelCommand* cmd = PanelCommand::captureStart(isolineParams, "edit Contours text");
 	QString strn;
+
+	setIgnoreBoxSliderEvents(true);
 	if (isolineParams->VariablesAre3D()){
 		float thetaVal = thetaEdit->text().toFloat();
 		while (thetaVal > 180.f) thetaVal -= 360.f;
@@ -717,6 +719,7 @@ void IsolineEventRouter::confirmText(bool /*render*/){
 	//
 	guiSetTextChanged(false);
 	PanelCommand::captureEnd(cmd, isolineParams);
+	setIgnoreBoxSliderEvents(false);
 	updateTab();
 }
 
